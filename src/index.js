68323e2650f2628407bcd8028ae4a0ca7a9c8bde
ryo.nakano <ryo.nakano@nadai.jp>
2019-11-28 22:41:53 +0900

    CSSファイルをApp.scssに統一

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {
 } from 'react-router-dom'
 import { Provider } from 'react-redux';
 //import configureStore from './store/configureStore';
-
+import './App.scss'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
