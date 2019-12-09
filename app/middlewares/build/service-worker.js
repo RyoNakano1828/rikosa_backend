9a9a13531bd81bd6dc7f0c4b619f3cbd4e20573a
ryo.nakano <ryo.nakano@nadai.jp>
2019-12-09 20:59:59 +0900

    これはどうでしょうか

--- app/middlewares/build/service-worker.js
+++ app/middlewares/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.768d6b9f495b0839e4f992013c0a584d.js"
+  "/precache-manifest.b91d40ac06b7aef3dfc8039df56895f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
