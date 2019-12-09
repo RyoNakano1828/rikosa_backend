b46fbd1cecc0e3700339ea3964983a6348e3e665
ryo.nakano <ryo.nakano@nadai.jp>
2019-12-09 22:33:21 +0900

    homepageを./に変更

--- app/middlewares/build/service-worker.js
+++ app/middlewares/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b91d40ac06b7aef3dfc8039df56895f6.js"
+  "./precache-manifest.0eda174ddb9efdd62a1cc48a854f9ff2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
